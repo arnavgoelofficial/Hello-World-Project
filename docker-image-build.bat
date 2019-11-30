ba0ed617a33b8b5e6da2a1d17614f0ffb41c6e34
arnavgoelofficial <30868606+arnavgoelofficial@users.noreply.github.com>
2019-11-30 14:04:27 +0530

    Delete docker-image-build.bat

--- docker-image-build.bat
+++
@@ -1,4 +0,0 @@
-SET PATH=%PATH%;C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin
-SET IMAGE_NAME=gcr.io/handle-books/gpymicro-image:%BUILD_NUMBER%
-cd c:\skillvelocity\courses\NU\Cloud\devops\PyMicroservices\Source
-gcloud builds submit --tag %IMAGE_NAME% .
